423ab6a5bf2946444cb570644bee6e72072d3003
Nisharga <kabir@nexlent.com>
2024-09-01 21:57:50 +0600

    feat: setup

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -6,7 +6,7 @@ const Welcome = () => {
   return (
     <SafeAreaView>
       <Text className="text-blue-500 text-center text-4xl">
-        Welcome To Tripio 🙂
+        Welcome To Tripio 🙂🙂
       </Text>
     </SafeAreaView>
   );
