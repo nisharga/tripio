2cf953902af0b3068b97a8365962a6a2e6f94eb1
Nisharga <kabir@nexlent.com>
2024-09-04 10:30:59 +0600

    feat: feature/clerk-authentication

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,20 +1,46 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prettier/prettier */
 import { Image, ScrollView, Text, View } from "react-native";
 import { icons, images } from "@/constants";
-import { useState } from "react";
+import React, { useState } from "react";
 import InputField from "@/components/InputField";
 import CustomButton from "@/components/CustomButton";
 import OAuth from "@/components/OAuth";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { useSignIn } from "@clerk/clerk-expo";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
-  const onSignInPress = () => {
-    console.log(form.email, form.password);
-  };
+
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const onSignInPress = React.useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(root)/(tabs)/home");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form]);
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
